fe0fd4a93213861e048616af9c63884de9ed8a9f
Aissam-salman <nasawi.lmj@gmail.com>
2024-07-19 09:43:01 +0200

    config prod 3

--- components/NavBar.component.tsx
+++ components/NavBar.component.tsx
@@ -7,6 +7,7 @@ export const NavBarComponent = () => {
                     <a href="/" className="text-gray-300 hover:text-white">Home</a>
                     <a href="/about" className="text-gray-300 hover:text-white">About</a>
                     <a href="/contact" className="text-gray-300 hover:text-white">Contact</a>
+                    <a href="/contact" className="text-gray-300 hover:text-white">Contact</a>
                 </div>
             </div>
         </nav>
