283daf8c5d7a7b29cd1cfd6722d36b855591ae91
Aissam-salman <nasawi.lmj@gmail.com>
2024-07-20 13:44:19 +0200

    config prod 8

--- src/api/endpoint.ts
+++ src/api/endpoint.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const ENDPOINT_PROD = "https://13.38.227.186:8080/api/v1"
+const ENDPOINT_PROD = "https:///ec2-13-38-227-186.eu-west-3.compute.amazonaws.com/api/v1"
 // const ENDPOINT = "http://localhost:8080/api/v1";
 export const API = axios.create({
     baseURL: ENDPOINT_PROD,
